a7f01216bc8b96c74c6e97657cd5287fb20a929c
Arseni <kyliannox@gmail.com>
2025-04-02 04:49:20 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿string result = "";
+int number = 3302;
+int system = 16;
+
+while (number > 0)
+{
+    int remainder = number % system;
+    string symbol = remainder < 10
+        ? remainder.ToString()
+        : ((char)('A' + remainder - 10)).ToString();
+
+    result = symbol + result;
+    number = number / system;
+}
+
+Console.WriteLine(result);
